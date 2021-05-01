378a8c4fdc47d0bdc29b586658212c8fcaeb947a
SabirBugti <abugti532@gmail.com>
2021-05-01 23:41:53 +0500

    complete

--- helper/jwt.js
+++ helper/jwt.js
@@ -11,13 +11,15 @@ function authJwt() {
             { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/category(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+
             `${api}/users/login`,
             `${api}/users/signUp`,
         ]
     });
 }
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
+    if (!payload.isAdmin) {
         done(null, true)
     }
 
